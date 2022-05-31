1506d4320fdd5703999893f223df7e73cea99634
stefi <stefaniaman314@gmail.com>
2022-05-31 17:22:57 +0300

    Design refactored.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OpenCallComponent } from './open-call/open-call.component';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: '', component: LandingPageComponent },
-	{ path: 'open-call', component: OpenCallComponent }
+  { path: '', component: LandingPageComponent }
 ];
 
 @NgModule({
