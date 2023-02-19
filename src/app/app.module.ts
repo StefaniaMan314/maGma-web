94114a85385c3a0ee1cf6acd1c0a796ba178e1e3
stefi <stefaniaman314@gmail.com>
2023-02-19 16:22:38 +0200

    Add gallery with images.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ShowcasePageComponent } from './showcase-page/showcase-page.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { GalleryModule } from  'ng-gallery';
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { ShowcasePageComponent } from "./showcase-page/showcase-page.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,15 +15,10 @@ import { GalleryModule } from  'ng-gallery';
     ShowcasePageComponent,
     GalleryComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-    AppRoutingModule,
-    GalleryModule
-  ],
+  imports: [BrowserModule, RouterModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
